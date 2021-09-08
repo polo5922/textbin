7d9a4ad377cc0520d050a2a47b41ed9a5ade3c09
paul barbaux <paul.barbaux@gmail.com>
2021-09-08 10:43:10 +0200

    Add files via upload

---
+++ changelog.md
@@ -0,0 +1,14 @@
+# Version 1.0 [07/09/2021] - [14:00]
+
+- first realease of the web apps
+- possibility to save, duplicate, and share with the link to another user
+
+# Version 1.1 [07/09/2021] - [15:00]
+
+- added shorcut
+- raw button and function
+
+# Version 1.2 [07/09/2021] - [16:00]
+
+- added the non posibility to select other element in raw
+- added changelog
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const fs = require("fs");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
@@ -15,24 +16,64 @@ admin.initializeApp({
 const db = admin.firestore();
 
 app.get("/", (req, res) => {
-  code = `Welcome to TextBin!
+  addVue("acceuil");
+  if (process.platform === "darwin") {
+    code = `Welcome to TextBin!
   
-  use the commands in the top right corner
-  to create a new file to share with others.
+use the commands in the top right corner
+to create a new file to share with others.
 
-  ## SHORCUTS
-  you can do Ctrl + Shift + S to save
-  you can do Ctrl + Shift + D to dupicate
-  you can do Ctrl + Shift + N to start a new text
-  you can do Ctrl + Shift + R to get the raw value`;
-  res.render("code-display", { code, language: "plaintext", page: "acceuil" });
+## SHORCUTS
+you can do ⌘ + Shift + S to save
+you can do ⌘ + Shift + D to dupicate
+you can do ⌘ + Shift + N to start a new text
+you can do ⌘ + Shift + R to get the raw value
+
+## CUSTOMISATION 
+you can customize what langage-hightlight you want with 
+http://textbin.polo5922.com/code-{langageWanted}`;
+  } else {
+    code = `Welcome to TextBin!
+  
+use the commands in the top right corner
+to create a new file to share with others.
+
+## SHORCUTS
+you can do Ctrl + Shift + S to save
+you can do Ctrl + Shift + D to dupicate
+you can do Ctrl + Shift + N to start a new text
+you can do Ctrl + Shift + R to get the raw value
+
+## CUSTOMISATION 
+you can customize what langage-hightlight you want with 
+http://textbin.polo5922.com/code-{langageWanted}`;
+  }
+  res.render("code-display", { code, language: "markdown", page: "acceuil" });
 });
 
 app.get("/new", (req, res) => {
+  addVue("new");
   res.render("new");
 });
 
+app.get("/changelog.md", (req, res) => {
+  addVue("changelog");
+  fs.readFile("./changelog.md", "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    code = data;
+    res.render("code-display", {
+      code,
+      language: "markdown",
+      page: "changelog",
+    });
+  });
+});
+
 app.post("/save", (req, res) => {
+  addVue("save");
   const value = req.body.value;
   console.log(value);
 
@@ -45,6 +86,7 @@ app.post("/save", (req, res) => {
 });
 
 app.get("/:id/duplicate", async (req, res) => {
+  addVue("duplicate");
   const id = req.params.id;
   try {
     const snapshot = await db.collection("files").doc(id).get("value");
@@ -57,6 +99,7 @@ app.get("/:id/duplicate", async (req, res) => {
 });
 
 app.get("/:id/raw", async (req, res) => {
+  addVue("raw");
   const id = req.params.id;
   try {
     const snapshot = await db.collection("files").doc(id).get("value");
@@ -68,7 +111,23 @@ app.get("/:id/raw", async (req, res) => {
   }
 });
 
+app.get("/:id-:language", async (req, res) => {
+  addVue("langage");
+  const id = req.params.id;
+  const language = req.params.language;
+  console.log(language);
+  try {
+    const snapshot = await db.collection("files").doc(id).get("value");
+    const data = snapshot.data();
+    console.log(data.value);
+    res.render("code-display", { code: data.value, id, language });
+  } catch (e) {
+    res.redirect("/");
+  }
+});
+
 app.get("/:id", async (req, res) => {
+  addVue("viewDocument");
   const id = req.params.id;
   try {
     const snapshot = await db.collection("files").doc(id).get("value");
@@ -79,4 +138,21 @@ app.get("/:id", async (req, res) => {
     res.redirect("/");
   }
 });
+
+async function addVue(type) {
+  console.log(type);
+  const snapshot = await db.collection("global").doc("vue").get(type);
+  const data = snapshot.data();
+  console.log(data[type]);
+  if (data[type] === undefined) {
+    obj = `{ ${type} : 1}`;
+  } else {
+    obj = `{ ${type} : ${data[type] + 1}}`;
+  }
+  console.log(obj);
+  eval("var newObj =" + obj);
+  console.log(newObj);
+  db.collection("global").doc("vue").update(newObj);
+}
+
 app.listen(3000);
